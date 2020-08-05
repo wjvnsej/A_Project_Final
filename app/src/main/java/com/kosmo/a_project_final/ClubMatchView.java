5e0e529720e5dc0e126a48fc65cf955077c16144
wjdtkdgur95 <wjdtkdgur95@gmail.com>
2020-08-05 22:24:13 +0900

    2020-08-05 JSH

--- app/src/main/java/com/kosmo/a_project_final/ClubMatchView.java
+++ app/src/main/java/com/kosmo/a_project_final/ClubMatchView.java
@@ -22,10 +22,10 @@ public class ClubMatchView extends AppCompatActivity {
         TextView g_snameTxt = (TextView)findViewById(R.id.g_snameTxt);
         g_snameTxt.setText(intent.getStringExtra("g_sname"));
 
-//        MapView mapView = new MapView(this);
-//
-//        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-//        mapViewContainer.addView(mapView);
+        MapView mapView = new MapView(getApplicationContext());
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
 
     }
 }
